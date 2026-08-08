e2f73b795bc915e242e7486de4db6969d2a22b7d
ahmad <175308714+ahmad345-hub@users.noreply.github.com>
2026-08-08 16:46:28 +0300

    feat: add user listing

---
+++ WebApplication1/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+
+namespace WebApplication1.Controllers
+{
+    public class UsersController : Controller
+    {
+
+        ApplicationDbContext context = new ApplicationDbContext();
+        public ViewResult Index()
+        {
+            var Users = context.Users.ToList();
+            return View(Users);
+        }
+
+        public ViewResult Creat()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+       public DbSet<User>Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(
+                "Data Source=.;Database=CRUD;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Application Name=\"SQL Server Management Studio\";Command Timeout=0"
+            );
+        }
+    }
+}
+
